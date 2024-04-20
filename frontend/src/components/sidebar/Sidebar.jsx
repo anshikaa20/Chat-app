d35a5bede4510f73bf212963849b5619ca774161
anshikaa20 <anshika.522375@gmail.com>
2024-04-20 18:35:49 +0530

    2nd part completed -ui design

---
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,24 @@
+
+import Conversations from './Conversations';
+import LogoutButton from './LogoutButton';
+import SearchInput from './SearchInput';
+
+const Sidebar = () => {
+  return (
+    <div  className='border-r border-slate-500 p-4 flex flex-col'>
+
+      <SearchInput/>
+      
+      <div className='border-r border-slate-500 p-4 flex flex-col'></div>
+      <Conversations />
+      <LogoutButton />
+
+          	{/*
+       			<LogoutButton />*/}
+       	
+            
+    </div>
+ )
+}
+
+export default Sidebar
