e9ee1646b7612de2164973874dc274ad643459f0
anshikaa20 <anshika.522375@gmail.com>
2024-04-24 22:08:18 +0530

    3rd part complete

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -55,14 +55,14 @@ export const getMessages = async(req,res)=>{
             participants:{$all:[senderId,userToChatId]},
         }).populate("messages"); //NOT REFRENCE BUT ACTUAL MESSAGES
 
-        if(!conversation) res.status(200).json([]);
+        if(!conversation) return res.status(200).json([]);
 
         const messages= conversation.messages;
 
         res.status(200).json(messages);
 
     } catch (error) {
-        console.log("Error in getMessages controller:", error.message)
+        console.log("Error in getMessages controller:", error.message);
         res.status(500).json({error:"Internal server error"});
         
     }
